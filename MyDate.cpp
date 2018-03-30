9dbef6e3315bc80667ea545fbdf53522332eaefc
Yookiely <zihang1998115@126.com>
2018-03-30 18:33:33 +0800

    fuck wanyan

--- MyDate.cpp
+++ MyDate.cpp
@@ -1,136 +1,209 @@
 #include "MyDate.h"
 #include <sstream>
-#define EPOCH_YEAR 1900
+#include <cmath>
 #define SECOND_OF_DAY 86400
-#define PING 365
-#define RUN 366
-#define TOEPOCH 25567
 using namespace std;
-MyDate::MyDate(long long i){
-    days=TOEPOCH+i/SECOND_OF_DAY;//按天储存
+
+bool isLeapYear(int year){
+    if(year>1752&&year%100==0){
+        if(year%400==0){
+            return true;
+        }else{
+            return false;
+        }
+    }else if(year%4==0){
+        return true;
+    }else{
+        return false;
+    }
 }
+
+int getMonthDay(int month,bool isLeapYear){
+    switch(month){
+    case 1:case 3:case 5:case 7:case 8:case 10:case 12:
+        return 31;
+    case 4:case 6:case 9:case 11:
+        return 30;
+    case 2:
+        if(isLeapYear)return 29;
+        else return 28;
+    }
+
+}
+
+bool validate(int year ,int month ,int day){
+    if(year==1752&&month==9&&day>2&&day<14){
+        return false;
+    }else if(year>=-9998&&year<=9999&&month>=1&&month<=12&&day>=1&&day<=getMonthDay(month,isLeapYear(year))){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+
+int getYearDay(int year){
+    if(isLeapYear(year)) return 366;
+    else return 365;
+}
+
+int getDays(int year,int month,int day){
+    long days = 0;
+    if(year>1970){
+        for(int y=1970;y<year;++y){
+            days=days+getYearDay(y);
+        }
+    }else{
+        for(int y=1969;y>=year;--y){
+            days=days-getYearDay(y);
+        }
+    }
+    for(int m = 1;m<month;++m){
+        days=days+getMonthDay(m,isLeapYear(year));
+    }
+    days =days+day-1;
+    if(year==1752){
+        if(month==9){
+            if(day<14){
+                days+=11;
+            }
+        }else if(month<9){
+            days+=11;
+        }
+    }else if(year<1752){
+        days+=11;
+    }
+    return days;
+}
+
+void getDate(int& year,int& month,int&day,long days){
+    int daysOfYear;
+    if(days>=0){
+        year=1970;
+        daysOfYear=getYearDay(year);
+        while(days>=daysOfYear){
+            year++;
+            days-=daysOfYear;
+            daysOfYear=getYearDay(year);
+        }
+        }else{
+            if(days<getDays(1752,9,14)){
+                days-=11;
+            }
+            year=1970;
+            while(days<0){
+                year--;
+                daysOfYear=getYearDay(year);
+                days+=daysOfYear;
+
+            }
+        }
+        month = 1;
+        days++;
+        for(int m=1;m<=12;++m){
+            int daysOfMonth = getMonthDay(m,isLeapYear(year));
+            if(days>daysOfMonth){
+                days-=daysOfMonth;
+                month++;
+            }else{
+                break;
+            }
+        }
+        day=days;
+    }
+
+MyDate::MyDate(long long i){
+    days = i/SECOND_OF_DAY;
+    if(days<-4371596||days>2932896){
+        days=0;
+    }
+} //seconds since UNIX epoch
 
 MyDate::MyDate(int x,int y,int z){
-    year=x;
-    month=y;
-    day=z;
-
-    int countYear=x-EPOCH_YEAR;
-
-    int  numberOfRun = (countYear+3)/4-1;
-    int  numberOfPing = countYear - numberOfRun;
-
-
-    days = days+numberOfRun*RUN + numberOfPing*PING;//计算平闰年总天数
-    days = days+monthNumber(year,month);
-    days = days+z-1;//按天储存
+    if(validate(x,y,z)){
+        days = getDays(x,y,z);
+    }else{
+        days = 0;
+    }
 }
+
 MyDate MyDate::add(int i) const{
-    return MyDate((i+days-TOEPOCH)*86400LL);//转换成秒创建新的对象。
+    return MyDate((i+days)*86400LL);//转换成秒创建新的对象。
 }
+
 MyDate MyDate::subtract(int i) const{
-    return MyDate((days-i-TOEPOCH)*86400LL);//转换成秒创建新的对象。
+    return MyDate((days-i)*86400LL);//转换成秒创建新的对象。
 }
+
 int MyDate::subtract(const MyDate& date ) const{
     return (this->days - date.days);//返回天数
 }
-bool MyDate::equals(const MyDate& date) const{
-    if(this->days==date.days)
-    {
-        return true;
-    }else{
-        return false;
-    }//对天数进行一个判断,下同
-}
-bool MyDate::lessThan(const MyDate& date) const{
-    if(this->days<date.days)
-    {
-        return true;
-    }else{
-        return false;
-    }
-}
-bool MyDate::lessThanOrEquals(const MyDate& date) const{
-    if(this->days<=date.days)
-    {
-        return true;
-    }else{
-        return false;
-    }
-}
-bool MyDate::greaterThan(const MyDate& date) const{
-    if(this->days>date.days)
-    {
-        return true;
-    }else{
-        return false;
-    }
-}
-bool MyDate::greaterThanOrEquals(const MyDate& date) const{
-    if(this->days>=date.days)
-    {
-        return true;
-    }else{
-        return false;
-    }
-}
-string MyDate::toString() const
-{
-    if(year!=1900){
-        return myToString(1900,days,1,1);
-    }else{
-            return myToString(year,days,month,day);
-    }
-}
-int MyDate::monthNumber(int year,int month) const{//私有函数计算由月份转换而来的天数
-    int a[13];
-    a[0]=0;a[1]=31;a[2]=59;a[3]=90;a[4]=120;a[5]=151;a[6]=181;a[7]=212;a[8]=243;a[9]=273;a[10]=304;a[11]=334;a[12]=365;
-    if(((year%100==0&&year%400==0)||(year%100!=0&&year%4==0))&&month>2){
-        return a[month-1]+1;
-    }else{
-        return a[month-1];
-    }
-}
-string MyDate::myToString(int year, int days,int month,int day) const{//为了实现toString写的骚函数。。。。
-    int a[13];
-    a[0]=0;a[1]=31;a[2]=59;a[3]=90;a[4]=120;a[5]=151;a[6]=181;a[7]=212;a[8]=243;a[9]=273;a[10]=304;a[11]=334;a[12]=365;
-    int i =1;
-    int myMonth = 1;
-    while(i){
-        if(((year)%100==0&&(year)%400==0)||((year)%100!=0&&(year)%4==0)){
-            if(days>=(a[12]+1)){
-                days=days-a[12]-1;
-                year++;
 
-            }
-            else{i=0;}
-        }else{
-            if(days>=a[12]){
-                days=days-a[12];
-                year++;
-            }
-            else{i=0;}
-        }
-    }
-    for(int i=12;i>1;i--){
-        if(year%100==0&&year%400==0){a[i]++;}
-        if(year%100!=0&&year%4==0){a[i]++;}
-    }
-    for(int i=0;i<13;i++){
-        if(days<a[i]){
-                myMonth=i;
-                days=days-a[i-1];
-                break;
-        }
-    }
-    day=days+1;
+bool MyDate::equals(const MyDate& date) const{
+    return this->days==date.days;//对天数进行一个判断,下同
+}
+
+bool MyDate::lessThan(const MyDate& date) const{
+    return this->days<date.days;
+}
+
+bool MyDate::lessThanOrEquals(const MyDate& date) const{
+    return this->days<=date.days;
+}
+
+bool MyDate::greaterThan(const MyDate& date) const{
+    return this->days>date.days;
+}
+
+bool MyDate::greaterThanOrEquals(const MyDate& date) const{
+    return this->days>=date.days;
+}
+
+bool MyDate::operator<(const MyDate& dt) const{
+    return MyDate::lessThan(dt);
+}
+bool MyDate::operator<=(const MyDate& dt) const{
+    return MyDate::lessThanOrEquals(dt);
+}
+bool MyDate::operator>(const MyDate& dt) const{
+    return MyDate::greaterThan(dt);
+}
+bool MyDate::operator==(const MyDate& dt) const{
+    return MyDate::equals(dt);
+}
+MyDate MyDate::operator+(int days) const{
+    return MyDate::add(days);
+}
+MyDate MyDate::operator-(int days) const{
+    return MyDate::subtract(days);
+}
+int MyDate::operator-(const MyDate& dt) const{
+    return MyDate::subtract(dt);
+}
+std::string MyDate::toString() const{
+    int year ,month,day=0;
+    getDate(year,month,day,this->days);
     std::stringstream c;
+    if(year<0){
+        c<<"-";
+        year=abs(year);
+    }
+    if(year>=-999&&year<=999){
+        c<<"0";
+        if(year>=-99&&year<=99){
+            c<<"0";
+            if(year>=-9&&year<=9){
+                c<<"0";
+            }
+        }
+    }
         c<<year;
         c<<"-";
-        if(myMonth<10){
+        if(month<10){
             c<<"0";
         }
-        c<<myMonth;
+        c<<month;
         c<<"-";
         if(day<10){
             c<<"0";
@@ -139,4 +212,22 @@ string MyDate::myToString(int year, int days,int month,int day) const{//为了实现
         string d = c.str();
         return d;
 }
+
+std::ostream& operator<<(std::ostream& os,const MyDate& dt){
+    os<<dt.toString();
+    return os;
+}
+
+int MyDate::getWeekday() const{
+    int weekday = (days-3)%7;
+    if(weekday>0){
+        return weekday;
+    }else if(weekday==0){
+        return 7;
+    }else{
+        return weekday+7;
+    }
+}
+
+
 
